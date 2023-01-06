51d41315fa2977a7f4a73dcbfc4d90d95bddf79c
Matthias Holoch <mholoch@gmail.com>
2023-01-06 20:09:51 +0100

    add publishing cpu temp, refactor topic names

--- bmp280_to_mqtt.py
+++ bmp280_to_mqtt.py
@@ -6,24 +6,35 @@ import paho.mqtt.client as mqtt
 import time
 
 # config:
-broker='127.0.0.1'
-port=1883
-topic_prefix="h10/floor1/living_room/bmp280"
-clientid='python-mqtt-bmp280'
+broker = "127.0.0.1"
+port = 1883
+client_id = "mqtt_on_pi"
+topic_prefix = "h10/floor1/living_room"
+status_topic = f"{topic_prefix}/{client_id}/status"
+bmp280_topic = f"{topic_prefix}/bmp280"
+pi_topic = f"{topic_prefix}/pi"
+cpu_temp_path = "/sys/class/thermal/thermal_zone0/temp"
 
 # BMP280 sensor
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(board.I2C(), address=0x76)
 
 # MQTT client
-client=mqtt.Client(clientid)
-client.will_set(f"{topic_prefix}/status", "offline", qos=2, retain=True)
+client=mqtt.Client(client_id)
+client.will_set(status_topic, "offline", qos=2, retain=True)
 client.connect(broker, port)
 client.loop_start()
-client.publish(f"{topic_prefix}/status", "online", qos=2, retain=True)
+client.publish(status_topic, "online", qos=2, retain=True)
 
 while True:
-        client.publish(f"{topic_prefix}/temperature", "{:.2f}".format(bmp280.temperature), qos=0, retain=False)
-        client.publish(f"{topic_prefix}/pressure", "{:.2f}".format(bmp280.pressure), qos=0, retain=False)
+        client.publish(f"{bmp280_topic}/temperature", "{:.2f}".format(bmp280.temperature), qos=0, retain=False)
+        client.publish(f"{bmp280_topic}/pressure", "{:.2f}".format(bmp280.pressure), qos=0, retain=False)
+
+        cpu_temp_celsius = None
+        with open(cpu_temp_path) as cpu_temp_file:
+                cpu_temp_celsius = int(cpu_temp_file.read()) / 1000.0
+                print(cpu_temp_celsius)
+        client.publish(f"{pi_topic}/cpu_temperature", cpu_temp_celsius, qos=0, retain=False)
+
         time.sleep(20);
 
 client.disconnect()
