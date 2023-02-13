c59499f3249f2be694b35b2ed13f1acaff691496
Matthias Holoch <mholoch@gmail.com>
2023-02-13 16:26:42 +0100

    add systemd service for the script

---
+++ service.nix
@@ -0,0 +1,24 @@
+{ config, lib, pkgs, ... }:
+
+let
+  # The package itself. It resolves to the package installation directory.
+  piLocalMqttClient = pkgs.callPackage ./package.nix {};
+
+  # An object containing user configuration (in /etc/nixos/configuration.nix)
+  cfg = config.services.pi-local-mqtt-client;
+in {
+  options.services.pi-local-mqtt-client.enable = lib.mkEnableOption "pi-local-mqtt-client";
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.pi-local-mqtt-client = {
+      description = "python client for publishing local info via mqtt";
+      after = [ "network-online.target" ];
+      wantedBy = [ "network-online.target" ];
+      serviceConfig = {
+        ExecStart = "${piLocalMqttClient}/bin/client";
+        Restart = "always";
+        RestartSec = "5";
+    };
+    };
+  };
+}
